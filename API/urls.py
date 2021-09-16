8aff41a78234276afd8e31012e2f18c59b545fc9
Tom Virgo <tomcvirgo@gmail.com>
2021-09-16 17:48:59 +0100

    add user serializer and endpoint

--- API/urls.py
+++ API/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='api-index'),
+    path('users', views.UserList.as_view(), name='user-list')
 ]
--- API/views.py
+++ API/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from .serilalizers import UserSerializer
 
 def index(request):
     return HttpResponse('<h1>Welcome to the api...</h1>')
+
+
+class UserList(APIView):
+    # permission_classes = [IsAuthenticated,]
+    def get(self, request, format=None):
+        restaurants = User.objects.all()
+        serializer = UserSerializer(restaurants, many=True)
+        return Response(serializer.data)
+
+    
