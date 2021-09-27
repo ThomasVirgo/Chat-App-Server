2ec2a6e637b8773ad86b3633c86e2e86a8aec0ff
Tom Virgo <tomcvirgo@gmail.com>
2021-09-27 10:05:04 +0100

    add first name last name to registration

--- API/serilalizers.py
+++ API/serilalizers.py
@@ -5,7 +5,7 @@ from .models import FriendRequest
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ['username', 'id', 'email', 'last_login', 'date_joined', 'groups', 'friends']
+        fields = ['username', 'id', 'email', 'last_login', 'date_joined', 'groups', 'friends', 'first_name', 'last_name']
 
 class FriendRequestSerializer(serializers.ModelSerializer):
     class Meta:
